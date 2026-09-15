ccbb426f8030733bfe35de3300b807d9567a1533
keruch <53012408+keruch@users.noreply.github.com>
2026-09-15 20:07:43 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,30 @@
+#include <cctype>
+
+class Solution {
+public:
+
+    bool isPalindrome(string s) {
+        for (int l = 0, r = s.size()-1; l < r; ) {
+            auto lc = tolower(s[l]);
+            auto rc = tolower(s[r]);
+
+            if ((lc > 'z' || lc < 'a') && (lc < '0' || lc > '9')){
+                l++;
+                continue;
+            }
+
+            if ((rc > 'z' || rc < 'a') && (rc < '0' || rc > '9')) {
+                r--;
+                continue;
+            }
+
+            if (lc != rc) {
+                return false;
+            }
+
+            r--;
+            l++;
+        }
+        return true;
+    }
+};
